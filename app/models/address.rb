21da35ddbec276cd87b7b12c2058090ef775460a
JAAR91 <jaarriagaramos@gmail.com>
2022-01-14 17:36:42 -0600

    create test for address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :user
+  
+  validates :calle, presence: true
+  validates :ext_number, presence: true
+  validates :int_number, presence: true
+  validates :postal_code, presence: true
+  validates :colonia, presence: true
+  validates :municipio, presence: true
+  validates :estado, presence: true
+  validates :pais, presence: true
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Address, type: :model do
     user.addresses.create(calle: "calle test", ext_number: "1", int_number: "1", postal_code: "12345", colonia: "col test", municipio: "tested", estado: "dc", pais: "mexico")
     expect(user.addresses.length).to eq(5)
   end
+
+  it 'all values are required' do
+    user = User.create(username: 'jaar', password: "joseisj99")
+    user.addresses.create(calle: "", ext_number: "1", int_number: "1", postal_code: "12345", colonia: "col test", municipio: "tested", estado: "dc", pais: "mexico")
+    user.addresses.create(calle: "calle test", ext_number: "", int_number: "1", postal_code: "12345", colonia: "col test", municipio: "tested", estado: "dc", pais: "mexico")
+    user.addresses.create(calle: "calle test", ext_number: "1", int_number: "", postal_code: "12345", colonia: "col test", municipio: "tested", estado: "dc", pais: "mexico")
+    user.addresses.create(calle: "calle test", ext_number: "1", int_number: "1", postal_code: "", colonia: "col test", municipio: "tested", estado: "dc", pais: "mexico")
+    user.addresses.create(calle: "calle test", ext_number: "1", int_number: "1", postal_code: "12345", colonia: "", municipio: "tested", estado: "dc", pais: "mexico")
+    user.addresses.create(calle: "calle test", ext_number: "1", int_number: "1", postal_code: "12345", colonia: "col test", municipio: "", estado: "dc", pais: "mexico")
+    user.addresses.create(calle: "calle test", ext_number: "1", int_number: "1", postal_code: "12345", colonia: "col test", municipio: "tested", estado: "", pais: "mexico")
+    user.addresses.create(calle: "calle test", ext_number: "1", int_number: "1", postal_code: "12345", colonia: "col test", municipio: "tested", estado: "dc", pais: "")
+    expect(user.addresses.length).to eq(8)
+  end
+
 end
